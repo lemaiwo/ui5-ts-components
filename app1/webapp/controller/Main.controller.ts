e0d894c1ff1983bcfec5e922b66c24762392c78f
Wouter Lemaire <wouterlemaire120@hotmail.com>
2023-11-03 22:03:19 +0000

    first commit

---
+++ app1/webapp/controller/Main.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace be.wl.app1.controller
+ */
+export default class Main extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
